5a94c68f58e549c19972df594a7b18fcf9bcfbbc
PrakashSinghRawat-git <prakashsinghrawatuk5ile@gmail.com>
2023-11-13 16:50:55 +0530

    added confirmation for leaving

--- app/inspect/page.js
+++ app/inspect/page.js
@@ -1,6 +1,46 @@
-import React from "react";
+"use client";
+import React,{ useEffect, useState } from "react";
 import Main from "@/components/Main";
+import { auth } from "@/app/config/firebase";
+import Login from "@/components/LogIn";
+
+
+const handleBeforeUnload = (event) => {
+  event.preventDefault();
+  event.returnValue = 'Are you sure you want to leave? Your changes may not be saved.';
+};
+
 const page = () => {
+    const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+   
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+      } else {
+        setUser(null);
+      }
+      setIsLoading(false);
+    });
+
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+      unsubscribe();
+    };
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (!user) {
+    return <Login />;
+  }
     return (
         <div>
             <div className=" w-full ">
--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
-'use client'
+"use client";
+import Link from "next/link";
 import { useEffect, useState } from "react";
 import { auth } from "./config/firebase";
 import Login from "@/components/LogIn";
-import { useRouter } from 'next/navigation';
+
 
 const handleBeforeUnload = (event) => {
   event.preventDefault();
@@ -12,9 +13,10 @@ const handleBeforeUnload = (event) => {
 export default function Home() {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const router = useRouter();
 
   useEffect(() => {
+   
+
     window.addEventListener('beforeunload', handleBeforeUnload);
 
     const unsubscribe = auth.onAuthStateChanged((authUser) => {
@@ -40,18 +42,16 @@ export default function Home() {
     return <Login />;
   }
 
-  const handleInspectClick = () => {
-    window.removeEventListener('beforeunload', handleBeforeUnload);
-    router.push('/inspect');
-  };
-
   return (
-    <section className="bg-white w-[100vw] flex flex-col justify-center items-center  ">
-      <h1 className="font-bold text-2xl ">Cars Karkhana</h1>
+    <section className="bg-white w-[100vw] flex flex-col justify-center items-center">
+      <h1 className="font-bold text-2xl">Cars Karkhana</h1>
       <p>Vehicle Information</p>
-      <button onClick={handleInspectClick} className={`text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2  `}>
+      <Link
+        href="/inspect"
+        className={`text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2`}
+      >
         Inspect
-      </button>
+      </Link>
     </section>
   );
 }
